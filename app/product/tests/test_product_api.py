04c9b630321aa4ef1419f24e6e2e7eb6b431cb10
Pavel Denisov <pavelvan50@gmail.com>
2020-09-28 19:34:17 +0100

    Added tests and implementation of creating products endpoint

--- app/product/tests/test_product_api.py
+++ app/product/tests/test_product_api.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user_model
 from django.urls import reverse
 from django.test import TestCase
 
@@ -30,3 +31,57 @@ class PublicProductAPITests(TestCase):
 
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
         self.assertEqual(resp.data, serializer.data)
+
+
+class PrivateProductAPITests(TestCase):
+    """Test the privatly availbale products API"""
+
+    def setUp(self):
+        self.superuser = get_user_model().objects.create_superuser(
+            email='testsuperusermail@gmail.com',
+            password='testsuperpass'
+        )
+        self.client = APIClient()
+        self.client.force_authenticate(user=self.superuser)
+
+    def test_create_new_product_need_authentication(self):
+        """Test that creating new product needs authentication"""
+        payload = {'name': 'newproduct', 'description': 'newdescr'}
+        client2 = APIClient()
+        resp = client2.post(PRODUCTS_URL, payload)
+
+        self.assertEqual(resp.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_create_new_product_need_superuser(self):
+        """Test that creating new product needs authentication"""
+        nonsuper_user = get_user_model().objects.create_user(
+            email='testemail@gmail.com',
+            password='testpasstest'
+        )
+        client2 = APIClient()
+        client2.force_authenticate(user=nonsuper_user)
+
+        payload = {'name': 'newproduct', 'description': 'newdescr'}
+        resp = client2.post(PRODUCTS_URL, payload)
+
+        self.assertEqual(resp.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_create_new_product_successful(self):
+        """Test that creating new product by superuser is working correctly"""
+        payload = {'name': 'newproduct2', 'description': 'newdescr2'}
+        resp = self.client.post(PRODUCTS_URL, payload)
+
+        exists = Product.objects.filter(
+            name=payload['name'],
+            description=payload['description']
+        ).exists()
+
+        self.assertEqual(resp.status_code, status.HTTP_201_CREATED)
+        self.assertTrue(exists)
+
+    def test_create_new_product_invalid(self):
+        """Test creating new product with invalid payload"""
+        payload = {'name': '', 'description': ''}
+        resp = self.client.post(PRODUCTS_URL, payload)
+
+        self.assertEqual(resp.status_code, status.HTTP_400_BAD_REQUEST)
--- app/product/views.py
+++ app/product/views.py
@@ -1,14 +1,44 @@
-from rest_framework import viewsets, mixins
+from rest_framework.response import Response
+from rest_framework import viewsets, mixins, status
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAdminUser
+from rest_framework.decorators import action
 
 from core.models import Product
 from product import serializers
 
 
-class ProductViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
-    """Manage products in database"""
+class _CustomProductPermission(IsAdminUser):
+    protected_methods = ['POST',]
+
+    def has_permission(self, request, view):
+        if request.method in self.protected_methods:
+            return super().has_permission(request, view)
+        return True
+
+
+class _CustomProductAuthentication(TokenAuthentication):
+    protected_methods = ['POST',]
+
+    def has_authentication(self, request, view):
+        if request.method in self.protected_methods:
+            return super().has_authentication(request, view)
+        return True
+
+
+class ProductViewSet(viewsets.GenericViewSet,
+                     mixins.ListModelMixin,
+                     mixins.CreateModelMixin):
+    """Viewset for listing and creating products in database"""
     queryset = Product.objects.all()
     serializer_class = serializers.ProductSerializer
+    permission_classes = (_CustomProductPermission,)
+    authentication_classes = (_CustomProductAuthentication,)
 
     def get_queryset(self):
         """Return all products in needed order"""
         return self.queryset.order_by('-name')
+
+    def perform_create(self, serializer):
+        """Special method for overriding post request for creating new products"""
+        serializer.save()
